a7ab3fe5a7d1bc9b8646a6a5ef45f2f86ba60851
Sujin Lee <pasdesujin@gmail.com>
2016-07-11 16:41:03 -0400

    connect to the server

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express();
+var port = 3000;
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, function (err) {
+  if (err) {
+    throw err;
+  } else {
+    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
+  }
+});
