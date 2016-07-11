89d53cd3005487abf7d88f14242a02d3856d7f55
Sujin Lee <pasdesujin@gmail.com>
2016-07-11 18:07:13 -0400

    add port for heroku. don't forget this part

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
+;
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
