e1242090a7487ca7471791c51a13d63c72f7abc4
Sujin Lee <pasdesujin@gmail.com>
2016-07-12 11:05:13 -0400

    add bodyparser and post works

--- client/app.js
+++ client/app.js
@@ -1,14 +1,13 @@
 angular.module('app', []) // register firebase as a module in application
 .controller('MainController', function ($scope, PokemonsFactory) {
-
   PokemonsFactory.getAll()
   .then(function(pokemons) {
     $scope.pokemons = pokemons;
   });
 
-  // $scope.addPokemon = function () {
-  //   PokemonsFactory.addPokemon($scope.newPokemon);
-  // }
+  $scope.addPokemon = function () {
+    PokemonsFactory.addPokemon($scope.newPokemon);
+  }
 
 }).factory('PokemonsFactory', function ($http) {
   //var pokemons = [{name:'sujin'}];
@@ -21,12 +20,13 @@ angular.module('app', []) // register firebase as a module in application
       console.log(resp.data);
       return resp.data;
     });
-  }
-  var addPokemon = function (newPokemon) {
+  };
+
+  var addPokemon = function (pokemon) {
     return $http({
       method: 'POST',
       url: '/api/pokemon',
-      data: newPokemon
+      data: pokemon
     })
     .then(function (resp) {
       return resp;
--- server.js
+++ server.js
@@ -4,12 +4,14 @@ var app = express();
 var mongoose = require('mongoose');
 var path = require('path');
 var Pokemon = require('./pokemonModel');
+var bodyParser = require('body-parser');
 
 //required for deployment
 var port = process.env.PORT || 3000;
-var mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:pokemon';
+var mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/pokemonList';
 mongoose.connect(mongoUri);
 
+app.use(bodyParser.json());
 app.use(express.static('client'));
 
 app.get('/', function (req, res) {
@@ -17,18 +19,23 @@ app.get('/', function (req, res) {
 });
 
 
+//dummy database
+var data = [{"name":'Honeybell'},{"name":"sujin"}];
 //api
 app.get('/api/pokemon', function (req, res) {
-  res.json([{name:'Honeybell'}]);
-  // Pokemon.find({}, function(err, result) {
-  //   res.send(200, result);
-  //   return result
-  // })
+  res.json(data);
+   // Pokemon.find({}, function(err, result) {
+   //  console.log(result)
+   //   res.send(200, result);
+   //   //return result
+   // })
 });
 
 app.post('/api/pokemon', function (req, res) {
-  console.log(req);
-  res.json(req)
+  console.log('req b',req.body);
+  data.push(req.body);
+  console.log('data' , data)
+  res.send(201)
 });
 
 app.listen(port, function (err) {
@@ -38,3 +45,5 @@ app.listen(port, function (err) {
     console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
   }
 });
+
+module.exports = app;
