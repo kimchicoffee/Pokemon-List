f07515796851249b1b37d2f7c7f230fbd9603cdd
Sujin Lee <pasdesujin@gmail.com>
2016-07-22 20:42:24 -0400

    add static google map

--- client/pokemon/pokemon.controller.js
+++ client/pokemon/pokemon.controller.js
@@ -3,6 +3,12 @@ angular.module('app')
     PokemonFactory.getOnePokemon()
     .then(function(pokemon) {
       $scope.pokemon = pokemon;
+      var mapProp = {
+      center:new google.maps.LatLng(42.3601,-71.060093),
+      zoom:5,
+      mapTypeId:google.maps.MapTypeId.ROADMAP
+      };
+      var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
     });
   })
   .factory('PokemonFactory', function ($stateParams,$http) {
