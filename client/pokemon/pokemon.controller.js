760ad237a2f3becbf44bef7004254dccdf987c69
Sujin Lee <pasdesujin@gmail.com>
2016-07-22 21:01:44 -0400

    try to fix google map bug

--- client/pokemon/pokemon.controller.js
+++ client/pokemon/pokemon.controller.js
@@ -1,15 +1,19 @@
 angular.module('app')
   .controller('pokemonCtrl', function ($scope, PokemonFactory) {
-    PokemonFactory.getOnePokemon()
-    .then(function(pokemon) {
-      $scope.pokemon = pokemon;
-      var mapProp = {
-      center:new google.maps.LatLng(42.3601,-71.060093),
-      zoom:5,
-      mapTypeId:google.maps.MapTypeId.ROADMAP
-      };
-      var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    });
+    var getOnePokemon = function() {
+      PokemonFactory.getOnePokemon()
+        .then(function(pokemon) {
+        $scope.pokemon = pokemon;
+        var mapProp = {
+        center:new google.maps.LatLng(42.3601,-71.060093),
+        zoom:5,
+        mapTypeId:google.maps.MapTypeId.ROADMAP
+        };
+        new google.maps.Map(document.getElementById("googleMap"),mapProp);
+        console.log('map is created')
+      });
+    };
+    getOnePokemon();
   })
   .factory('PokemonFactory', function ($stateParams,$http) {
     var getOnePokemon = function() {
