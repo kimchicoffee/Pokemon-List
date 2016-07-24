c74c04ed6b020e3119f91d12fbb92f9398c1ff24
Sujin Lee <pasdesujin@gmail.com>
2016-07-24 10:41:14 -0400

    delete map feature

--- client/pokemon/pokemon.controller.js
+++ client/pokemon/pokemon.controller.js
@@ -4,12 +4,6 @@ angular.module('app')
       PokemonFactory.getOnePokemon()
       .then(function(pokemon) {
         $scope.pokemon = pokemon;
-        var mapOptions = {
-          zoom: 4,
-          center: new google.maps.LatLng(40.0000, -98.0000),
-          mapTypeId: google.maps.MapTypeId.TERRAIN
-        }
-        $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
       });
     };
     getOnePokemon();
